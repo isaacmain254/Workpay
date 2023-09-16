8a53b0e2e26a8f097334b6ce877737dd55a3e1bb
isaacmain254 <isaacmain72@gmail.com>
2023-09-16 09:48:03 +0300

    modified models

--- account/models.py
+++ account/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # user profile
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(upload_to='users/%Y/%m/%d/', default='images/default-profile-image.jpeg', blank=True, null=True)
     phone_number = PhoneNumberField(blank=True)
     country = models.CharField(max_length=200, blank=True)
@@ -17,27 +17,32 @@ class Profile(models.Model):
         return f' {self.user.username}'
     
 
-class Project(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=250, blank=True)
-    description = models.TextField(null=True, blank=True)
-    project_image = models.ImageField(upload_to='projects/', blank=True, null=True)
-
-    def __str__(self):
-        return f'{self.title}'
-    
 class Bio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
     profession = models.CharField(max_length=250, null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user}'
-
+    
 class Skill(models.Model):
-    user_bio = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.ForeignKey(Bio, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}'
+
+class Project(models.Model):
+    bio = models.ForeignKey(Bio, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, blank=True)
+    description = models.TextField(null=True, blank=True)
+    project_image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.title}'
+    
